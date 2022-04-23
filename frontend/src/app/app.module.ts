60cd5a822114ae5945ad5d799704167126a7d60e
Thiago Vilela <thiagosilvavilela@gmail.com>
2022-04-22 23:10:25 -0300

    Creating base components for the frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AddGameComponent } from './components/add-game/add-game.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GameComponent } from './components/game/game.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddGameComponent,
+    GameListComponent,
+    GameComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
